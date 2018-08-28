bddd00720a9918d79dcd663be4fab3a7bb210ded
kemijibola <oluwakemi.awosile@apmis.ng>
2018-08-28 05:37:59 +0100

    scaffold

---
+++ PMSRedefined/Allocation.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PMSRedefined
+{
+    partial class Allocation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.splitContainer2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(686, 426);
+            this.splitContainer1.SplitterDistance = 395;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.groupBox1);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.button2);
+            this.splitContainer2.Panel2.Controls.Add(this.comboBox1);
+            this.splitContainer2.Panel2.Controls.Add(this.button1);
+            this.splitContainer2.Panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.splitContainer2_Panel2_Paint);
+            this.splitContainer2.Size = new System.Drawing.Size(395, 426);
+            this.splitContainer2.SplitterDistance = 248;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 426);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Lecturers";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 16);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(242, 407);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedValueChanged += new System.EventHandler(this.listBox1_SelectedValueChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1, 288);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(140, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Deallocate Student";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(3, 80);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Allocate Student";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox2.Location = new System.Drawing.Point(0, 0);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(287, 426);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Students";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(3, 16);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(281, 407);
+            this.checkedListBox1.TabIndex = 1;
+            // 
+            // Allocation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 426);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Allocation";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lecturer/Student Allocation";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Allocation_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ PMSRedefined/Allocation.cs
@@ -0,0 +1,187 @@
+﻿using PMSRedefined.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PMSRedefined
+{
+    public partial class Allocation : Form
+    {
+        public Allocation()
+        {
+            InitializeComponent();
+        }
+
+        private void Allocation_Load(object sender, EventArgs e)
+        {
+            pmsstoreEntities entties = new pmsstoreEntities();
+            var queryLecturers = (from d in entties.tbl_CSLecturer
+                select d).ToList();
+            this.listBox1.DataSource = queryLecturers;
+            this.listBox1.ValueMember = "Guid";
+            this.listBox1.DisplayMember = "Name";
+            this.listBox1.SelectedIndex = -1;
+
+            var queryStudents = (from p in entties.tbl_Student
+                select p).ToList();
+            this.checkedListBox1.DataSource = queryStudents;
+            this.checkedListBox1.DisplayMember = "Name";
+            this.checkedListBox1.ValueMember = "GUID";
+
+            var querySession = (from p in entties.tbl_Session
+                select p).ToList();
+            this.comboBox1.DataSource = querySession;
+            this.comboBox1.DisplayMember = "SessionName";
+            this.comboBox1.ValueMember = "GUID";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pmsstoreEntities entties = new pmsstoreEntities();
+         
+            foreach (var j in this.checkedListBox1.CheckedItems)
+            {
+                tbl_Student studentItem = (tbl_Student) j;
+                tbl_CSLecturer lecturerItem = (tbl_CSLecturer) this.listBox1.SelectedItem;
+                int sessionId =Convert.ToInt32(this.comboBox1.SelectedValue);
+                //check if the student has been allocated to any lecturer before in selected session
+                var queryCheck = (from p in entties.tbl_Allocation
+                    where p.StudentID == studentItem.GUID && p.SessionID == sessionId
+                    select p).FirstOrDefault();
+                if (queryCheck != null)
+                {
+                    //this student is already assigned to this or another lecturer this session, therefore continue to the next student
+                   continue;
+                }
+
+
+                tbl_Allocation allocation = new tbl_Allocation();
+                allocation.DateAllocated = DateTime.Now;
+                allocation.LecturerID = lecturerItem.GUID;
+                allocation.SessionID = Convert.ToInt32(this.comboBox1.SelectedValue);
+                allocation.StudentID = studentItem.GUID;
+             
+                entties.tbl_Allocation.Add(allocation);
+            }
+            entties.SaveChanges();
+        }
+
+        private void RefreshItems()
+        {
+            try
+            {
+                pmsstoreEntities entties = new pmsstoreEntities();
+
+                for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+                {
+                    foreach (var j in this.checkedListBox1.CheckedItems)
+                    {
+
+                        this.checkedListBox1.SetItemChecked(i, false);
+                        break;
+                    }
+                }
+
+                tbl_CSLecturer lecturerItem = (tbl_CSLecturer)this.listBox1.SelectedItem;
+                int sessionId = Convert.ToInt32(this.comboBox1.SelectedValue);
+                var queryAllocation = (from p in entties.tbl_Allocation
+                                       where p.LecturerID == lecturerItem.GUID && p.SessionID == sessionId
+                                       select p).ToList();
+                foreach (var allocation in queryAllocation)
+                {
+                    foreach (var j in this.checkedListBox1.Items)
+                    {
+                        tbl_Student studentItem = (tbl_Student)j;
+                        if (studentItem.GUID == allocation.StudentID)
+                        {
+                            this.checkedListBox1.SelectedItem = j;
+                            int index = this.checkedListBox1.SelectedIndex;
+                            this.checkedListBox1.SetItemChecked(index, true);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+        private void listBox1_SelectedValueChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                pmsstoreEntities entties = new pmsstoreEntities();
+
+                for (int i = 0; i < this.checkedListBox1.Items.Count; i++)
+                {
+                    foreach (var j in this.checkedListBox1.CheckedItems)
+                    {                  
+                        
+                        this.checkedListBox1.SetItemChecked(i, false);
+                        break;
+                    }
+                }
+          
+                tbl_CSLecturer lecturerItem = (tbl_CSLecturer)this.listBox1.SelectedItem;
+                   int sessionId =Convert.ToInt32(this.comboBox1.SelectedValue);
+                var queryAllocation = (from p in entties.tbl_Allocation
+                    where p.LecturerID == lecturerItem.GUID && p.SessionID == sessionId
+                    select p).ToList();
+                foreach (var allocation in queryAllocation)
+                {
+                    foreach (var j in this.checkedListBox1.Items)
+                    {
+                        tbl_Student studentItem = (tbl_Student)j;
+                        if (studentItem.GUID == allocation.StudentID)
+                        {
+                            this.checkedListBox1.SelectedItem = j;
+                            int index = this.checkedListBox1.SelectedIndex;                                 
+                            this.checkedListBox1.SetItemChecked(index,true);
+                            break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pmsstoreEntities entties = new pmsstoreEntities();
+
+            foreach (var j in this.checkedListBox1.CheckedItems)
+            {
+                tbl_Student studentItem = (tbl_Student)j;
+                tbl_CSLecturer lecturerItem = (tbl_CSLecturer)this.listBox1.SelectedItem;
+                int sessionId = Convert.ToInt32(this.comboBox1.SelectedValue);
+                //check if the student has been allocated to any lecturer before in selected session
+                var queryCheck = (from p in entties.tbl_Allocation
+                                  where p.StudentID == studentItem.GUID && p.SessionID == sessionId
+                                  select p).FirstOrDefault();
+                if (queryCheck != null)
+                {
+                   //remove
+                    entties.tbl_Allocation.Remove(queryCheck);                   
+                }             
+            }
+            entties.SaveChanges();
+            RefreshItems();
+        }
+
+        private void splitContainer2_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
